6588130e09af997e0a03e7228ff64665c403490b
dokdogalmaegi <52159385+dokdogalmaegi@users.noreply.github.com>
2020-06-04 18:03:14 +0900

    embed(봇 설명) 추가 주석, 실시간 시간 표시

--- SmartSpace_Discord/Program.cs
+++ SmartSpace_Discord/Program.cs
@@ -9,6 +9,8 @@ namespace SmartSpace_Discord
     class Program
     {
         private DiscordSocketClient client; // 전역 함수 client 변수 할당 Ex) Java - Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        EmbedBuilder builder = new EmbedBuilder();
         static void Main(string[] args)
         {
             new Program().MainAsync().GetAwaiter().GetResult(); // MainAsync을 동기로 실행하지만 실제로는 비동기를 실행합니다. - MainAsync문 실행 -> GetAwaiter값을 받아올때까지 대기 -> GetResult 값을 받아오면 반환 <- 이 과정을 비동기로 처리
@@ -54,22 +56,19 @@ namespace SmartSpace_Discord
 
         private async Task MessageReceivedAsync(SocketMessage message) // SocketMessage - Type을 의미, message
         {
-            Random rand = new Random();
-            EmbedBuilder builder = new EmbedBuilder();
-            builder.WithThumbnailUrl("https://cdn.discordapp.com/attachments/558305011256393739/717986439261978686/unknown.png");
-            builder.AddField(name: "명령어", value: "!help - 현재 있는 명령어와 기본정보를 모두 보여줍니다.\n!주사위, !dice - 6개의 숫자를 랜덤으로 출력해줍니다." );
+            builder.WithThumbnailUrl("https://cdn.discordapp.com/attachments/558305011256393739/717986439261978686/unknown.png"); // embed 썸네일사진 설정
+            builder.AddField(name: "명령어", value: "!help - 현재 있는 명령어와 기본정보를 모두 보여줍니다.\n!주사위, !dice - 6개의 숫자를 랜덤으로 출력해줍니다." ); // embed 설정
+            builder.WithTimestamp(DateTime.Now); // DateTime.Now로 현재 시간을 받아와 WithTimesteamp로 embed에 표시함
             builder.WithColor(Color.Blue);
             if (message.Author.Id == client.CurrentUser.Id) // 봇이 명령어를 쳤을 때 아무 리턴값을 주지 않는다. - 예외처리
                 return;
-
             if (message.Content == "!주사위" || message.Content == "!dice") // 유저가 !주사위 또는 !dice라는 명령어를 디스코드에 입력하게 되면 
             {
-                await message.Channel.SendMessageAsync(rand.Next(1, 6).ToString()); // Random 함수를 사용하여 랜덤값을 반환하게 한다.
+                String returnmsg = String.Format("주사위의 결과 : {0}", rand.Next(1, 6));
+                await message.Channel.SendMessageAsync(returnmsg); // Random 함수를 사용하여 랜덤값을 반환하게 한다.
             }
             if (message.Content == "!help")
-            {
                 await message.Channel.SendMessageAsync("", false, builder.Build());
-            }
         }
     }
 }
